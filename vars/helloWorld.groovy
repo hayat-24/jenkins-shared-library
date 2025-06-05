d3bf4aa5b640f746269c2345618e136e8d5fd9d3
hayat-24 <digital.crea24@gmail.com>
2025-06-05 17:29:39 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,8 @@
-def call(String name = 'world') {
-    echo "Hello, ${name} from shared library!"
-}
+#!/usr/vin/env groovy
+import groovy.json.*
+    def call(body) {
+        println ("Hello World !!!")
+    }
+//def call(String name = 'world') {
+//    echo "Hello, ${name} from shared library!"
+//}
