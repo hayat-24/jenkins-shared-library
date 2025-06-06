fed5503b5fffe6d8f9d1237dcb31fe7bfbd965e5
hayat-24 <digital.crea24@gmail.com>
2025-06-06 09:21:03 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,16 +1,11 @@
-//def call(String name = 'world') {
-// echo "Hello, ${name} from shared library!"
-//}
-//#/usr/bin/env groovy
-
-import groovy.json.*
- def call(body) {
+def call(body) {
   def mapVars = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = mapVars
-  body ()
+  body()
 
   def name = mapVars.name
-  def action = maVars.action
+  def action = mapVars.action
 
-  println( action + " " + name + "!!")
+  echo "${action} ${name}!!"
+}
